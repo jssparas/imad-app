079e5d503d4b71beeae41f0e24d7c18f1e16ee2d
Paras <jssparas@gmail.com>
2017-09-06 08:24:09 +0530

    added template function to generate html file

--- server.js
+++ server.js
@@ -5,12 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article = {
+	title: 'Article One',
+	heading: 'this is article one',
+	date: 'published on 4 sep',
+	body: `<p>
+		this is body for article one. This is the only article in this web-app. Ideally there should be two more articles, but....ok lets create article two. But that will be the last one.
+		</p>`
+}
+
+function createHtmlTemplate(data)
+{
+	var htmlTemplate = `<html>
+				<head>
+					<title>
+						${data.title}
+					</title>
+					<style>
+						body{
+							background-color:red
+						}
+					</style>
+				</head>
+				<body>
+					<h1>
+						${data.heading}
+					</h1>
+					${data.body}
+				</body>
+			    </html>`
+
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
-	res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+	res.send(createHtmlTemplate(article));
 });
 
 app.get('/article-two', function(req, res){
